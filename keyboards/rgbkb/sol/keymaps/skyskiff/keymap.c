14b10dcc1c2202a01e1f3e55ad55c683123a9050
htprww <htprww@gmail.com>
2020-05-22 16:08:25 -0400

    Update keymap.c

--- keyboards/rgbkb/sol/keymaps/skyskiff/keymap.c
+++ keyboards/rgbkb/sol/keymaps/skyskiff/keymap.c
@@ -36,8 +36,8 @@ enum custom_keycodes { RGBRST = SAFE_RANGE, RGB_MENU, RGB_SAVE, CTRL_K, CTRL_O, 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAC] = LAYOUT( \
-    KC_ESC,   KC_1,     KC_2,   KC_3,    KC_4,    KC_5,   KC_GRV, KC_BSLS,  KC_6,     KC_7,   KC_8,    KC_9,    KC_0,    KC_DEL,  \
-    KC_TAB,   KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,   SETMWE, FUNC,   KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_ESC,   KC_1,     KC_2,   KC_3,    KC_4,    KC_5,   KC_GRV,  KC_BSLS, KC_6,     KC_7,   KC_8,    KC_9,    KC_0,    KC_DEL,  \
+    KC_TAB,   KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,   SETMWE,  FUNC,    KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL,  KC_A,     KC_S,   KC_D,    KC_F,    KC_G,   KC_MINS, KC_EQL,  KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT,  KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,   KC_MUTE, RGB_TOG, KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),  \
     G(KC_TAB),G(KC_GRV),KC_LALT,KC_LGUI, FN1,     KC_SPC, KC_ENT,  KC_ENT,  KC_SPC,   FN2,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,\
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN1] = LAYOUT( \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
     KC_CAPS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_TILD, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PIPE, KC_F12,  \
-    _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_UNDS, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN,  KC_DQT, \
+    _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_UNDS, POINTER, ARROW,   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN,  KC_DQT, \
     _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_ASTR, RESET,   RGBRST,  KC_PPLS, KC_AMPR, KC_LT,   KC_GT,   KC_QUES, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
                                                  _______, _______, _______, _______),
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN2] = LAYOUT( \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
     KC_CAPS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_TILD, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PIPE, KC_F12,  \
-    _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_UNDS, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN,  KC_DQT, \
+    _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_UNDS, POINTER, ARROW,   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN,  KC_DQT, \
     _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_ASTR, RESET,   RGBRST,  KC_PPLS, KC_AMPR, KC_LT,   KC_GT,   KC_QUES, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
                                                  _______, _______, _______, _______)
