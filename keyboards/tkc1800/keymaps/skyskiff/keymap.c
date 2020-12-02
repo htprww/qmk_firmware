1e2e58ac89276324bac2bd47a5130708a62d8539
htprww <htprww@gmail.com>
2020-12-02 13:06:58 -0500

    a

--- keyboards/tkc1800/keymaps/skyskiff/keymap.c
+++ keyboards/tkc1800/keymaps/skyskiff/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_P7,   KC_P8,   KC_P9,   XXXXXXX, \
     MO(NAV), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT,           KC_P4,   KC_P5,   KC_P6,   C(KC_C), \
     KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,      KC_UP,       KC_P1,   KC_P2,   KC_P3,   XXXXXXX, \
-    KC_LCTL, KC_LBRC, KC_LALT,                   KC_SPC,                        KC_RCTL, MO(FN), KC_RGUI,          KC_LEFT, KC_DOWN, KC_RGHT,       KC_P0,   KC_PDOT, C(KC_V)  \
+    KC_LCTL, KC_RBRC, KC_LALT,                   KC_SPC,                        KC_RCTL, MO(FN), KC_RGUI,          KC_LEFT, KC_DOWN, KC_RGHT,       KC_P0,   KC_PDOT, C(KC_V)  \
   ),
   /* non layer */
   [NON] = LAYOUT(
