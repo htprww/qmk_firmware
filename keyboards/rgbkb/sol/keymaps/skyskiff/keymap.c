1665e257353aa9b4bd5bcaaa7e796ee2241a2861
htprww <htprww@gmail.com>
2021-04-23 22:59:34 -0400

    sol layout update

--- keyboards/rgbkb/sol/keymaps/skyskiff/keymap.c
+++ keyboards/rgbkb/sol/keymaps/skyskiff/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_M] = LAYOUT( \
     KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_F5,    KC_F8,    KC_6,     KC_7,   KC_8,     KC_9,    KC_0,     KC_DEL,     \
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_F7,    KC_F10,   KC_Y,     KC_U,   KC_I,     KC_O,    KC_P,     KC_BSPC,    \
-    MO(_N),  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_F8,    KC_F11,   KC_H,     KC_J,   KC_K,     KC_L,    KC_SCLN,  KC_QUOT,    \
+    MO(_N),  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,   KC_F8,    KC_F11,   KC_H,     KC_J,   KC_K,     KC_L,    KC_SCLN,  KC_QUOT,    \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_MUTE,  RGB_TOG,  KC_N,     KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,    \
-    MO(_L),  KC_GRV,  KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC, KC_ENT,   KC_ENT,   KC_SPC,   MO(_R), KC_RGUI,  KC_RALT, KC_BSLS,  TG(_L),    \
+    MO(_L),  KC_GRV,  KC_LCTL, KC_LALT, KC_LGUI,   KC_SPC, KC_DEL,   KC_DEL,   KC_SPC,   MO(_R), KC_RGUI,  KC_RALT, KC_BSLS,  TG(_L),    \
                                                    KC_SPC, KC_ENT,   KC_ENT,   KC_SPC),
 
     [_W] = LAYOUT( \
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_F10,   KC_F10,   KC_Y,     KC_U,   KC_I,     KC_O,    KC_P,     KC_BSPC,    \
     MO(_N),   KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_F11,   KC_F11,   KC_H,     KC_J,   KC_K,     KC_L,    KC_SCLN,  KC_QUOT,    \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_MUTE,  RGB_TOG,  KC_N,     KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,    \
-    MO(_L),  KC_GRV,  KC_LGUI, KC_LALT, KC_LCTL,  KC_SPC, KC_ENT,   KC_ENT,   KC_SPC,   MO(_R), KC_RCTL,  KC_RGUI, KC_BSLS,  TG(_L),    \
+    MO(_L),  KC_GRV,  KC_LGUI, KC_LALT, KC_LCTL,   KC_SPC, KC_DEL,   KC_DEL,   KC_SPC,   MO(_R), KC_RCTL,  KC_RGUI, KC_BSLS,  TG(_L),    \
                                                    KC_SPC, KC_ENT,   KC_ENT,   KC_SPC),
 
     [_G] = LAYOUT( \
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_F2,    KC_P4,    KC_Y,     KC_U,   KC_I,     KC_O,    KC_P,     KC_BSPC,    \
     MO(_N),   KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_F3,    KC_P1,    KC_H,     KC_J,   KC_K,     KC_L,    KC_SCLN,  KC_QUOT,    \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_MUTE,  RGB_TOG,  KC_N,     KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,    \
-    KC_LCTL,  KC_GRV,  KC_LBRC, KC_RBRC, KC_LALT,  KC_SPC, KC_ENT,   KC_ENT,   KC_SPC,   MO(_R), KC_RCTL,  KC_RGUI, KC_BSLS,  TG(_L),    \
+    KC_LCTL,  KC_GRV,  KC_LBRC, KC_RBRC, KC_LALT,  KC_SPC, KC_DEL,   KC_DEL,   KC_SPC,   MO(_R), KC_RCTL,  KC_RGUI, KC_BSLS,  TG(_L),    \
                                                    KC_SPC, KC_ENT,   KC_ENT,   KC_SPC),
 
     [_L] = LAYOUT( \
